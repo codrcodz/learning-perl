72ebcdd5e1b98c78cd23f5e3665684fc21544eed
codrcodz <cody.l.cochran@gmail.com>
2016-12-26 07:51:27 +0300

    filehandles

--- filehandles.pl
+++ filehandles.pl
@@ -9,3 +9,13 @@
 # open(SESAME, "| output-pipe-command")  # set up an output filter
 # open(SESAME, "input-pipe-command |")   # set up an input filter
 
+print STDOUT "Enter a number: ";         # ask for a number
+$first_num = <STDIN>;                    # input the number
+chop($first_num);                        # remove newline
+
+# alternately, this can be done in a two line
+print STDOUT "Enter another number: ";   # ask for a number
+chop($sec_num = <STDIN>);                # input number and remove newline
+
+# print the results
+print STDOUT "The numbers are ${first_num} and ${sec_num}.\n";
